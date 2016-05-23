80bcf0a96ae235f3583db1a316736ce482a58919
lengers <leo@engers.de>
2016-05-23 15:07:44 +0200

    Corrected business logic

--- src/com/dhbw/programming/Main.java
+++ src/com/dhbw/programming/Main.java
@@ -18,9 +18,9 @@ public class Main {
 	private static Scanner user_input;
 	private static int[] rollable;
 	private static int[] dice;
-	private static int[][] player;
 	static int[] numbers = new int[6];
-	static int points = 0;
+	static int playerCount;
+	static int pips;
 
 	public static void main(String[] args) {
 
@@ -57,9 +57,10 @@ public class Main {
 		}
 		Main.printDice(dice);
 
-		Main.userRequest();
-		Main.userRequest();
-		System.out.println("You scored " + IntStream.of(dice).sum() + " points.");
+		Main.userRequest(player);
+		Main.userRequest(player);
+		pips = IntStream.of(dice).sum();
+		System.out.println("You scored " + pips + " points.");
 	}
 
 	public static void printDice(int[] dice) {
@@ -68,7 +69,7 @@ public class Main {
 		System.out.println(" " + dice[0] + "  " + dice[1] + "  " + dice[2] + "  " + dice[3] + "  " + dice[4]);
 	}
 
-	private static void again(int[] dice) {
+	private static void again(int[] dice, Player player) {
 		System.out.println("Which dice do you want to roll again? You can choose from [" + rollable
 				+ "] (Enter in form of 1,3,4)");
 		String rollAgain = user_input.next();
@@ -82,15 +83,15 @@ public class Main {
 
 		Main.printDice(dice);
 
-		Main.getValue(dice);
+		Main.getValue(dice, player);
 	}
 
-	private static void userRequest() {
+	private static void userRequest(Player player) {
 		System.out.println("Do you want to go again? (y/n)");
 		String go_again = user_input.next();
 		go_again = "y";
 		if (go_again == "y") {
-			Main.again(dice);
+			Main.again(dice, player);
 		} else if (go_again == "n") {
 			System.out.println("Ok, your dice are final for this round.");
 		} else {
@@ -99,7 +100,8 @@ public class Main {
 		}
 	}
 
-	private static void getValue(int[] dice) {
+	private static void getValue(int[] dice, Player player) {
+		Data data = player.getData();
 		Arrays.fill(numbers, 0);
 		for (int i = 0; i < dice.length; i++) {
 			numbers[i - 1] += 1;
@@ -112,28 +114,116 @@ public class Main {
 		for (int i = 0; i < numbers.length; i++) {
 			if (numbers[i] >= 4) {
 				// Five-of-a-kind, called "Kniffel", results in 50 points
-				if (numbers[i] == 5 && player[1][15] == 0) {
-					player[0][15] = 50;
-					player[1][15] = 1;
+				if (numbers[i] == 5 && data.getKniffel().getLock() == false) {
+					player.setPoints(player.getPoints() + data.getKniffel().getPoints());
+					data.setKniffel(true);
 					System.out.println("You rolled a yahtzee!");
 					// Four-of-a-kind, add all pips as points
-				} else if (player[1][i] == 0) {
-					player[0][i] = IntStream.of(dice).sum();
-					player[1][i] = 1;
-					System.out.println("You rolled a four-of-a-kind!");
-				}
-			} else if (numbers[i] == 3) {
-				if (true) {
-
-				} else if (player[1][i] == 0) {
-					player[0][i] = IntStream.of(dice).sum();
-					player[1][i] = 1;
-					System.out.println("You rolled a three-of-a-kind!");
 				} else {
-					System.out.println("No field unlocked, error.");
+					switch (i) {
+					case 1:
+						if (data.getFourofakindOne().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setFourofakindOne(1, pips, true);
+							System.out.println("You rolled a four-of-a-kind!");
+						}
+						break;
+					case 2:
+						if (data.getFourofakindTwo().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setFourofakindTwo(2, pips, true);
+							System.out.println("You rolled a four-of-a-kind!");
+						}
+						break;
+					case 3:
+						if (data.getFourofakindThree().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setFourofakindThree(2, pips, true);
+							System.out.println("You rolled a four-of-a-kind!");
+						}
+						break;
+					case 4:
+						if (data.getFourofakindFour().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setFourofakindFour(2, pips, true);
+							System.out.println("You rolled a four-of-a-kind!");
+						}
+						break;
+					case 5:
+						if (data.getFourofakindFive().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setFourofakindFive(2, pips, true);
+							System.out.println("You rolled a four-of-a-kind!");
+						}
+						break;
+					case 6:
+						if (data.getFourofakindSix().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setFourofakindSix(2, pips, true);
+							System.out.println("You rolled a four-of-a-kind!");
+						}
+						break;
+					}
+
 				}
+				// if (player[1][i] == 0) {
+				// player[0][i] = IntStream.of(dice).sum();
+				// player[1][i] = 1;
+				// System.out.println("You rolled a four-of-a-kind!");
+				// }
+			} else if (numbers[i] == 3) {
+				if (pips < 2) {
+
+				} else
+					switch (i) {
+					case 1:
+						if (data.getThreeofakindOne().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setThreeofakindOne(1, pips, true);
+							System.out.println("You rolled a three-of-a-kind!");
+						}
+						break;
+					case 2:
+						if (data.getThreeofakindTwo().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setThreeofakindTwo(2, pips, true);
+						}
+						break;
+					case 3:
+						if (data.getThreeofakindThree().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setThreeofakindThree(3, pips, true);
+						}
+						break;
+					case 4:
+						if (data.getThreeofakindFour().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setThreeofakindFour(4, pips, true);
+						}
+						break;
+					case 5:
+						if (data.getThreeofakindFive().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setThreeofakindFive(5, pips, true);
+						}
+						break;
+					case 6:
+						if (data.getThreeofakindSix().getLock() == false) {
+							player.setPoints(player.getPoints() + pips);
+							data.setThreeofakindSix(6, pips, true);
+						}
+						break;
+					default:
+						System.out.println("No field unlocked, error.");
+						break;
+					}
+			} else if (numbers[0] == 1 && numbers[1] == 1 && numbers[2] == 1 && numbers[3] == 1 && numbers[4] == 1) {
+				data.setLittleStraight(true);
+				player.setPoints(player.getPoints() + data.getLittleStraight().getPoints());
+			} else if (numbers[1] == 1 && numbers[2] == 1 && numbers[3] == 1 && numbers[4] == 1 && numbers[5] == 1) {
+				data.setBigStraight(true);
+				player.setPoints(player.getPoints() + data.getBigStraight().getPoints());
 			}
 		}
 	}
-
 }
